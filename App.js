108950438711cd988ade37178e5a6d0d28a4de48
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2024-10-28 16:31:10 +0200

    Update .gitignore

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StyleSheet } from 'react-native';
 import HomePage from './components/screens/HomePage';
+
+const database = getDatabase(app);
 
 export default function App() {
 
-  
   return (
      <HomePage></HomePage>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
